5857dc299fe0c935065dab3b573c7e0e660de6a1
Pahlevon <rezaamaroof@gmail.com>
2025-07-10 17:12:17 +0330

    Pic+Card Fixed

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,19 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new CopyWebpackPlugin({
+  patterns: [
+    { 
+      from: path.resolve(__dirname, 'src/shared/images'), 
+      to: path.resolve(__dirname, 'dist/images') 
+    },
+    // favicon هم اگر نیاز است
+    { 
+      from: path.resolve(__dirname, 'src/shared/images/favicon.ico'), 
+      to: path.resolve(__dirname, 'dist') 
+    }
+  ]
+}),
     new miniCssExtractPlugin(),
     new CopyWebpackPlugin({
       patterns: [
@@ -35,66 +48,84 @@ module.exports = {
   },
   module: {
     rules: [
-      
-        {
-          mimetype: 'image/svg+xml',
-          scheme: 'data',
-          type: 'asset/resource',
-          generator: {
-            filename: 'icons/[hash].svg'
-          }
-        },
-        , {
-          test: /\.js$/,
-          exclude: [/node_modules/, /\.scss$/],
-          use: {
-            loader: 'babel-loader',
-            options: {
-              sourceType: 'module',
-              presets: ['@babel/preset-env']
-            }
-          }
-        },
-        {
-          test: /\.(scss)$/,
-          type: 'javascript/auto',
-          use: [
-            {
-              // Extracts CSS for each JS file that includes CSS loader: miniCssExtractPlugin.loader
-              loader: 'style-loader'
-            },
-            {
-              // Interprets `@import` and `url()` like `import/require()` and will resolve them
-              loader: 'css-loader'
-            },
-            {
-              // Loader for webpack to process CSS with PostCSS
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  plugins: [
-                    autoprefixer
-                  ]
-                }
-              }
-            },
-            {
-              // Loads a SASS/SCSS file and compiles it to CSS
-              loader: 'sass-loader',
-              options: {
-                sassOptions: {
-                  // Optional: Silence Sass deprecation warnings. See note below.
-                  silenceDeprecations: [
-                    'mixed-decls',
-                    'color-functions',
-                    'global-builtin',
-                    'import'
-                  ]
-                }
-              }
-            }
-          ]
+      // قانون جدید برای تصاویر (به غیر از SVG)
+      {
+        test: /\.(png|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext][query]'
         }
-      ]
+      },
+      // قانون موجود برای SVG (همین قانون فعلی شما)
+      {
+        mimetype: 'image/svg+xml',
+        scheme: 'data',
+        type: 'asset/resource',
+        generator: {
+          filename: 'icons/[hash].svg'
+        }
+      },
+      // بقیه قوانین...
+
+      {
+        mimetype: 'image/svg+xml',
+        scheme: 'data',
+        type: 'asset/resource',
+        generator: {
+          filename: 'icons/[hash].svg'
+        }
+      },
+      , {
+        test: /\.js$/,
+        exclude: [/node_modules/, /\.scss$/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            sourceType: 'module',
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(scss)$/,
+        type: 'javascript/auto',
+        use: [
+          {
+            // Extracts CSS for each JS file that includes CSS loader: miniCssExtractPlugin.loader
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                // Optional: Silence Sass deprecation warnings. See note below.
+                silenceDeprecations: [
+                  'mixed-decls',
+                  'color-functions',
+                  'global-builtin',
+                  'import'
+                ]
+              }
+            }
+          }
+        ]
+      }
+    ]
   }
 }
